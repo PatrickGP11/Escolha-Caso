8964949cbbb23f8438403946f8d5c92e59f1983a
PatrickGP11 <patrickgonsalves06@gmail.com>
2025-04-07 18:52:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+/* var pessoa = {
+    nome: "Patrick",
+    idade: 18,
+    tel: "(11)980134511",
+    bigode: true,
+    barba: false,
+
+}
+
+alert(`Nome: ${pessoa.nome}
+Idade: ${pessoa.idade}
+tel: ${pessoa.tel}`) */
+
+
+/* var carros = ["Celta", "Civic", "Brasilia", "Corsa", "HB20", "Onix"]
+
+Array
+
+alert(carros[carros.length - 1]) */
+
+
+/* var diaSemana = Number(prompt(`Digite um dia da semana 1 a 7`))
+
+switch(diaSemana) {
+    case 1:
+        alert("Domingo")
+        break
+    case 2:
+        alert("Segunda")
+        break
+    case 3:
+        alert("Terça")
+        break
+    case 4:
+        alert("Quarta")
+        break
+    case 5:
+        alert("Quinta")
+        break
+    case 6:
+        alert("Sexta")
+        break
+    case 7:
+        alert("Sábado")
+        break
+    default:
+        alert("Não existe esse dia da semana [ERRO]")
+        break
+} */
